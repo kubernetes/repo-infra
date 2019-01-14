505caa01ed708e0240f68b1f58e8737ac7f483ff
Hannes Hörl <hhorl@pivotal.io>
2019-01-14 11:09:18 +0000

    Configure travis to checkout the branch to be merged
    Signed-off-by: Maria Ntalla <mntalla@pivotal.io>

--- verify/test-repo-infra-subtree.sh
+++ verify/test-repo-infra-subtree.sh
@@ -20,16 +20,14 @@ set -o pipefail
 readonly WORKSPACE="$( mktemp -d )"
 trap 'rm -rf -- "$WORKSPACE"' EXIT
 
+[ "${TRAVIS:-}" = 'true' ] && {
+  git checkout "$BRANCH_TO_MERGE"
+}
+
 readonly REPO_INFRA_URL="$( git rev-parse --show-toplevel )"
 readonly REPO_INFRA_PATH="tools/repo-infra"
-
-REPO_INFRA_REV="$( git rev-parse 'HEAD' )"
-REPO_INFRA_REV_PARENT="$( git rev-parse 'HEAD^' )"
-[ "${TRAVIS:-}" = 'true' ] && {
-  REPO_INFRA_REV="$TRAVIS_COMMIT"
-  REPO_INFRA_REV_PARENT="$( git rev-parse "${REPO_INFRA_REV}^" )"
-}
-readonly REPO_INFRA_REV REPO_INFRA_REV_PARENT
+readonly REPO_INFRA_REV="$( git rev-parse 'HEAD' )"
+readonly REPO_INFRA_REV_PARENT="$( git rev-parse 'HEAD^' )"
 
 export REPO_INFRA_REV
 export REPO_INFRA_URL
